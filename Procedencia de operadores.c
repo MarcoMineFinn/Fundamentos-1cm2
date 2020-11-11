04345b89d13558c585950424b62b0a706fed5720
Marco Jasso <jassom546@gmail.com>
2020-11-10 18:21:40 -0600

    Tarea

---
+++ Procedencia de operadores.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+      float res = 1.0;
+      float re = 2.0;
+      float r = 3.0; 
+    
+    res = ((6.18/5.0)*(pow(((14.1-4)/2),2)));
+    re = ((8.9*34)/17);
+    r = (8-7);
+     
+              printf("El primer resulado es %f\n", res);
+              printf("El primer resultado es %f\n", re);
+              printf("El primer resultado es %f\n",r);
+   
+              
+ return 0;
+}
+
