73140a2876e73f6cdaf361f0fc3a0a67dd540e2f
Marco Jasso <jassom546@gmail.com>
2020-11-17 15:19:30 -0600

    Tarea

---
+++ tablas for.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int a;
+    int b;
+
+    for(b=1; b<=10; b++){ 
+
+     printf ("-_-_-_-_-_-_-\n");
+
+         for (a=1; a <= 10; a++){
+
+    printf ("%d X %d = %d\n",b,a,b*a);
+
+
+
+         }
+
+    }   
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
