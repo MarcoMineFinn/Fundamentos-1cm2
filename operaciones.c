56a84991790bb05e50cf192173939755cbb516cf
Marco Jasso <jassom546@gmail.com>
2020-11-26 00:20:02 -0600

    operaciones

---
+++ operaciones.c
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: marcojasso
+ *
+ * Created on 5 de noviembre de 2020, 07:44 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char** argv) {
+   
+  float res = 1.0;
+  float re = 2.0;
+  float r = 3.0; 
+
+res = ((6.18/5.0) * (pow(((14.1-4)/2),2)));
+re = ((8.9*34)/17);
+r = (8-7); 
+
+        printf ("El primer resultado es %f\n", res) ; 
+        
+        printf ("El primer resultado es %f\n", re) ;
+        
+        printf ("El primer resultado es %f\n", r) ;
+    return (EXIT_SUCCESS);
+}
+
+
