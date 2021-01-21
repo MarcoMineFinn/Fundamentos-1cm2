b6e083224ee138f53f06c6ec045461ada1b47d58
Marco Jasso <jassom546@gmail.com>
2021-01-21 12:07:53 -0600

    apuntadores 3

---
+++ apuntadores_3.c
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   apuntadores_3.c
+ * Author: marcojasso
+ *
+ * Created on 11 de enero de 2021, 05:29 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    float x, y, res;
+    
+    float *uno, *dos, *resul;
+    uno = &x; dos = &y; resul = &res;    
+    
+    printf("Digite un número para sumar: ");
+    scanf("%f",&x);
+    printf("Digite otro número para sumar:");
+    scanf("%f",&y);
+    *resul= *uno + *dos;
+    printf("El resultado de la suma es: %.3f", res);  
+    
+    return (EXIT_SUCCESS);
+}
+
