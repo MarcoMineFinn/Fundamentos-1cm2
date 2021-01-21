f61bbdd95ffcc69bb90442ae9cd172fe3b1e5bb8
Marco Jasso <jassom546@gmail.com>
2021-01-21 12:09:45 -0600

    control 1

---
+++ control_1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    int i;
+while(i <= 15){
+    printf("%i\n",i);
+    i++;
+}  
+    return (EXIT_SUCCESS);
+}
+
